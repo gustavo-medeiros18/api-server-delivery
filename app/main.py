8d20d7a286d6959c80792d5b5ec535a23f96909b
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-06-08 02:57:03 -0300

    aula 7 gravada

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-from app.routes.restaurante_routes import router_restaurantes
+# API Server Delivery
+api = FastAPI(title="API Server Delivery")
 
-api = FastAPI(
-    title="API Delivery"
-)
-api.include_router(router_restaurantes)
-
+# Decorator: maneira de adicionar informações
+# e comportamentos especiais a uma função.
 @api.get("/")
 def raiz():
-    resposta = "API Delivery funcionando"
-    
+    resposta = "Essa é a raiz da aplicação"
     return resposta
