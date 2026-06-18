567c3cb0803e1cd918c42e55fa367d5c840acf69
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-06-17 23:36:55 -0300

    preparo aula-8

--- app/schemas/restaurante_schema.py
+++ app/schemas/restaurante_schema.py
@@ -1,42 +1,8 @@
-from datetime import datetime
-
 from pydantic import BaseModel, Field
 
-class RestauranteCriacao(BaseModel):
-    nome: str = Field(
-        min_length=3,
-        max_length=100
-    )
-    rua: str = Field(
-        min_length=3,
-        max_length=255
-    )
-    bairro: str = Field(
-        min_length=3,
-        max_length=100
-    )
-    numero: int = Field(
-        gt=0
-    )
-    cidade: str = Field(
-        min_length=3,
-        max_length=100
-    )
-    categoria: str = Field(
-        min_length=3,
-        max_length=100
-    )
-
-class RestauranteResposta(BaseModel):
-    id: int
-    nome: str
-    rua: str
-    bairro: str
-    numero: int
-    cidade: str
-    categoria: str
-    created_at: datetime
-    updated_at: datetime
-
-    class Config:
-        from_attributes = True
+# classe NomeClasse(ClasseBase):
+#     campo_1
+#     campo_2
+#     campo_3
+class RestauranteCriacaoSchema(BaseModel):
+    
