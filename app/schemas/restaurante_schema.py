5f5bbd88b6511f301145b947df3613e9b1b6ee26
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-06-18 23:48:19 -0300

    preparo aula-8

--- app/schemas/restaurante_schema.py
+++ app/schemas/restaurante_schema.py
@@ -5,4 +5,9 @@ from pydantic import BaseModel, Field
 #     campo_2
 #     campo_3
 class RestauranteCriacaoSchema(BaseModel):
-    
+    nome: str
+    rua: str
+    bairro: str
+    numero: int
+    cidade: str
+    categoria: str
