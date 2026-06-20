96a490abf6b129896b0ec8861f9aa7d58d1ce500
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-06-20 15:38:15 -0300

    preparo aula-8

--- app/routes/restaurante_routes.py
+++ app/routes/restaurante_routes.py
@@ -1,12 +1,15 @@
 from fastapi import (
     APIRouter
 )
+from app.schemas.restaurante_schema import RestauranteCriacaoSchema
 
 router_restaurantes = APIRouter()
 
 @router_restaurantes.post(
     "/restaurantes"
 )
-def criar():
+def criar(dados_entrada_restaurante: RestauranteCriacaoSchema):
+    print(f"Dados recebidos: {dados_entrada_restaurante}")
+
     resposta = "Essa á e rota para criar um restaurante"
     return resposta
