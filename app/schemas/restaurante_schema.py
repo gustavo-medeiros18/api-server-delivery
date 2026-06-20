d1221603b9a70fb3067f1a6a9d387e9c6c91617a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-06-20 01:51:41 -0300

    preparo aula-8

--- app/schemas/restaurante_schema.py
+++ app/schemas/restaurante_schema.py
@@ -5,9 +5,29 @@ from pydantic import BaseModel, Field
 #     campo_2
 #     campo_3
 class RestauranteCriacaoSchema(BaseModel):
-    nome: str
-    rua: str
-    bairro: str
-    numero: int
-    cidade: str
-    categoria: str
+    # min_length = quantidade minima de caracteres para o campo
+    # max_length = quantidade maxima de caracteres para o campo
+    nome: str = Field(
+        min_length=3, 
+        max_length=100
+    )
+    rua: str = Field(
+        min_length=3, 
+        max_length=255
+    )
+    bairro: str = Field(
+        min_length=3, 
+        max_length=100
+    )
+    # gt = greater than (maior que)
+    numero: int = Field(
+        gt=0
+    )
+    cidade: str = Field(
+        min_length=3, 
+        max_length=100
+    )
+    categoria: str = Field(
+        min_length=3, 
+        max_length=100
+    )
