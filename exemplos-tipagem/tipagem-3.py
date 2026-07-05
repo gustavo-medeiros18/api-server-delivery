54a0a9131323b40034a1bd7e840c71a63c304685
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-07-05 19:42:04 -0300

    aula 9 sendo gravada

---
+++ exemplos-tipagem/tipagem-3.py
@@ -0,0 +1,9 @@
+def dobro_e_comprimento_sem_tipagem(numero, palavra):
+    dobro_numero = numero * 2
+    comprimento_palavra = len(palavra)
+
+    print(f"Dobro do número: {dobro_numero}")
+    print(f"Comprimento da palavra: {comprimento_palavra}")
+
+dobro_e_comprimento_sem_tipagem(10, "dev")
+dobro_e_comprimento_sem_tipagem(20, 30.54)
