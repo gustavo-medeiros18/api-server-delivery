2f324ed28aa5f9dd84f0107a1517b2d7c0766ec7
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-07-07 01:13:42 -0300

    aula 9 sendo gravada

--- app/schemas/restaurante_schema.py
+++ app/schemas/restaurante_schema.py
@@ -1,1 +1,24 @@
 from pydantic import BaseModel, Field
+
+class RestauranceCriacaoSchema(BaseModel):
+    nome: str = Field(
+        min_length=3,
+        max_length=100
+    )
+    rua: str = Field(
+        min_length=3,
+        max_length=255
+    )
+    bairro: str = Field(
+        min_length=3,
+        max_length=100
+    )
+    numero: int = Field()
+    cidade: str = Field(
+        min_length=3,
+        max_length=100
+    )
+    categoria: str = Field(
+        min_length=3,
+        max_length=100
+    )
