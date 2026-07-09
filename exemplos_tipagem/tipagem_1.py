42ee07af0b62bcd9a2de22f205bb340db78f099a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-07-08 22:36:25 -0300

    deleta copia da pasta de exemplos tipagem

--- exemplos_tipagem/tipagem_1.py
+++
@@ -1,9 +0,0 @@
-def multiplicar_sem_tipagem(x, y):
-	multiplicacao = x * y
-	return multiplicacao
-
-resultado_1 = multiplicar_sem_tipagem(10, 20)
-print(f"Resultado 1 sem tipagem: {resultado_1}")
-
-resultador_2 = multiplicar_sem_tipagem("dev", "cdc")
-print(f"Resultado 3 sem tipagem: {resultador_2}")
