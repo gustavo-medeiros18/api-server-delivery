f9766e00db1e184742c3dbcabaa9d69c47475159
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-07-21 20:19:56 -0300

    aula 10 sendo gravada

---
+++ app/models/restaurante_model.py
@@ -0,0 +1,14 @@
+from banco_de_dados import modelo_base
+from sqlalchemy import Column, String, Integer
+
+class RestauranteModel(modelo_base):
+    __tablename__ = "restaurantes"
+
+    id = Column(Integer)
+
+    nome = Column(String, nullable=False)
+    rua = Column(String, nullable=False)
+    bairro = Column(String, nullable=False)
+    numero = Column(Integer, nullable=False)
+    cidade = Column(String, nullable=False)
+    categoria = Column(String, nullable=False)
