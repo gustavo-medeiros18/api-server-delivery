4f9ce1f1cb4b98bd71fd8ff8d5524663e7a8b128
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-07-23 11:25:39 -0300

    começa a adicionar doc sobre parametros da classe column

--- app/models/restaurante_model.py
+++ app/models/restaurante_model.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String, Integer
 class RestauranteModel(modelo_base):
     __tablename__ = "restaurantes"
 
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
 
     nome = Column(String, nullable=False)
     rua = Column(String, nullable=False)
@@ -12,3 +12,5 @@ class RestauranteModel(modelo_base):
     numero = Column(Integer, nullable=False)
     cidade = Column(String, nullable=False)
     categoria = Column(String, nullable=False)
+
+    criado_em = Column(String)
