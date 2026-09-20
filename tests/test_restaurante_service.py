1e7ae8a47c700d903e8702c4712c1b8da260c5d1
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-09-20 20:06:42 -0300

    aula 1 modulo 6 sendo gravada

--- tests/test_restaurante_service.py
+++ tests/test_restaurante_service.py
@@ -1,8 +1,9 @@
 from unittest.mock import Mock
 from app.schemas.restaurante_schema import RestauranteCriacaoSchema
 from app.models.restaurante_model import RestauranteModel
+from app.services import restaurante_service
 
-def test_criar():
+def test_criar(monkeypatch):
     dados_entrada_exemplo = RestauranteCriacaoSchema(
         nome="Restaurance Central",
         rua="Rua do parque",
@@ -16,3 +17,7 @@ def test_criar():
     retorno_dao_simulada = Mock(spec=RestauranteModel)
 
     criar_dao_simulado = Mock(return_value=retorno_dao_simulada)
+
+    monkeypatch.setattr(restaurante_service.restaurante_dao, "criar", criar_dao_simulado)
+
+    resultado = restaurante_service.criar(sessao_simulada, dados_entrada_exemplo)
