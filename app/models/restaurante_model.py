915f3f26841cd3c131a3fe8cc7b2af370777e36e
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-09-20 21:22:48 -0300

    aula 1 modulo 6 gravada

--- app/models/restaurante_model.py
+++ app/models/restaurante_model.py
@@ -1,4 +1,5 @@
 from app.banco_de_dados import modelo_base
+from app.models.pedido_model import PedidoModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from datetime import datetime
--- tests/test_restaurante_service.py
+++ tests/test_restaurante_service.py
@@ -15,9 +15,21 @@ def test_criar(monkeypatch):
 
     sessao_simulada = Mock()
     retorno_dao_simulada = Mock(spec=RestauranteModel)
-
     criar_dao_simulado = Mock(return_value=retorno_dao_simulada)
 
     monkeypatch.setattr(restaurante_service.restaurante_dao, "criar", criar_dao_simulado)
 
     resultado = restaurante_service.criar(sessao_simulada, dados_entrada_exemplo)
+
+    modelo_dados_criado = criar_dao_simulado.call_args.args[1]
+
+    assert resultado is retorno_dao_simulada
+    assert isinstance(modelo_dados_criado, RestauranteModel)
+    assert modelo_dados_criado.nome == dados_entrada_exemplo.nome
+    assert modelo_dados_criado.rua == dados_entrada_exemplo.rua
+    assert modelo_dados_criado.bairro == dados_entrada_exemplo.bairro
+    assert modelo_dados_criado.numero == dados_entrada_exemplo.numero
+    assert modelo_dados_criado.cidade == dados_entrada_exemplo.cidade
+    assert modelo_dados_criado.categoria == dados_entrada_exemplo.categoria
+
+    criar_dao_simulado.assert_called_with(sessao_simulada, modelo_dados_criado)
